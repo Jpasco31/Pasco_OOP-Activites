1a273b76092bf9f52988699360bf6f3ab9feb588
Jericho Pasco <112312834+Jpasco31@users.noreply.github.com>
2023-09-23 17:00:24 +0800

    Commit version3 Employee Project

---
+++ EmployeeProject/src/employee/version3/CommissionEmployee.java
@@ -0,0 +1,52 @@
+package employee.version3;
+
+public class CommissionEmployee extends Employee{
+    private double totalSales;
+
+    public CommissionEmployee() {
+        this.totalSales = 0;
+    }
+
+    public CommissionEmployee(int empId, Name empName, Date empDateHired, Date empBirthDate) {
+        super(empId, empName, empDateHired, empBirthDate);
+        this.totalSales = 0;
+    }
+
+    public CommissionEmployee(int empId, Name empName, Date empDateHired, Date empBirthDate, double totalSales) {
+        super(empId, empName, empDateHired, empBirthDate);
+        this.totalSales = totalSales;
+    }
+
+    public double getTotalSales() {
+        return totalSales;
+    }
+
+    public void setTotalSales(double totalSales) {
+        this.totalSales = totalSales > 0 ? totalSales : 0;
+    }
+
+    public double computeSalary (){
+        if(this.totalSales < 50000){
+            return this.totalSales * 0.05;
+        } else if (this.totalSales >= 50000 && this.totalSales < 100000){
+            return this.totalSales * 0.2;
+        } else if (this.totalSales >= 100000 && this.totalSales < 500000){
+            return this.totalSales * 0.3;
+        } else {
+            return this.totalSales * 0.5;
+        }
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Commission Employee");
+        System.out.println(toString());
+        System.out.printf("Salary: %.2f\n\n" ,computeSalary());
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()
+                + String.format("\nTotal sales: %.2f", totalSales);
+    }
+}
---
+++ EmployeeProject/src/employee/version3/Date.java
@@ -0,0 +1,50 @@
+package employee.version3;
+
+public class Date {
+    private int day;
+    private int month;
+    private int year;
+
+    public Date(){
+        this.day = 1;
+        this.month = 1;
+        this.year = 2023;
+    }
+
+    public Date(int day, int month, int year) {
+        this.day = day > 0 && day <= 31 ? day: 1;
+        this.month = month > 0 && month <= 12 ? month : 1;
+        this.year = year > 0 ? year: 2023;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day > 31 || month < 0? 1 : day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month > 12 || month < 0? 1 : month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year < 0 ? 2023 : year;
+    }
+
+    @Override
+    public String toString() {
+        String[] monthString = {"JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"};
+
+        return String.format("%02d %s %04d", day, monthString[month-1], year);
+    }
+}
