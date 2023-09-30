163320403659b25626970f33091095c8c7b53799
Jericho Pasco <112312834+Jpasco31@users.noreply.github.com>
2023-09-30 15:12:17 +0800

    Employee Version 4 Latest Update - done!

--- EmployeeProject/src/employee/version4/EmployeeRoster.java
+++ EmployeeProject/src/employee/version4/EmployeeRoster.java
@@ -1,21 +1,24 @@
 package employee.version4;
 
-import java.util.ArrayList;
+import java.util.Objects;
 
 public class EmployeeRoster {
-    private ArrayList<Employee> empList = new ArrayList<Employee>();
+    public Employee[] empList;
     private int count = 0;
-    private int max;
+    private int max = 0;
+
 
     public EmployeeRoster() {
         this.max = 10;
+        empList = new Employee[this.max];
     }
 
     public EmployeeRoster(int max) {
         this.max = max;
+        empList = new Employee[this.max];
     }
 
-    public ArrayList<Employee> getEmpList() {
+    public Employee[] getEmpList() {
         return empList;
     }
 
@@ -30,8 +33,8 @@ public class EmployeeRoster {
     public void displayAllEmployee(){
         System.out.printf("%-10s | %-40s | %-30s | %-20s\n", "ID", "NAME", "TYPE", "SALARY");
         for (int i = 0; i < this.count; i++) {
-            Employee employee = empList.get(i);
-            String fullName = employee.getEmpName().getFirstName() + employee.getEmpName().getLastName() + employee.getEmpName().getMiddleInitial();
+            Employee employee = empList[i];
+            String fullName = employee.getEmpName().getFirstName() + " " + employee.getEmpName().getMiddleInitial() + " " + employee.getEmpName().getLastName();
             String employeeType = employee.getClass().getSimpleName();
             double salary;
 
@@ -56,7 +59,7 @@ public class EmployeeRoster {
     public int countHE(){
         int numOfHE = 0;
         for(int i = 0; i < this.count; i++){
-            if(empList.get(i) instanceof HourlyEmployee){
+            if(empList[i] instanceof HourlyEmployee){
                 numOfHE++;
             }
         }
@@ -66,7 +69,7 @@ public class EmployeeRoster {
     public int countPWE(){
         int numOfPWE = 0;
         for(int i = 0; i < this.count; i++){
-            if(empList.get(i) instanceof PieceWorkerEmployee){
+            if(empList[i] instanceof PieceWorkerEmployee){
                 numOfPWE++;
             }
         }
@@ -76,7 +79,7 @@ public class EmployeeRoster {
     public int countCE(){
         int numOfCE = 0;
         for(int i = 0; i < this.count; i++){
-            if(empList.get(i) instanceof CommissionEmployee){
+            if(empList[i] instanceof CommissionEmployee && !(empList[i] instanceof BasePlusCommissionEmployee)){
                 numOfCE++;
             }
         }
@@ -87,10 +90,118 @@ public class EmployeeRoster {
     public int countBPEC(){
         int numOfBPEC = 0;
         for(int i = 0; i < this.count; i++){
-            if(empList.get(i) instanceof BasePlusCommissionEmployee){
+            if(empList[i] instanceof BasePlusCommissionEmployee){
                 numOfBPEC++;
             }
         }
         return numOfBPEC;
+    }
+
+    public void displayHE(){
+        System.out.println("HOURLY EMPLOYEE LIST");
+        for(int i = 0; i < this.count; i++){
+            if(empList[i] instanceof HourlyEmployee){
+                ((HourlyEmployee) empList[i]).displayInfo();
+            }
+        }
+    }
+
+    public void displayPWE(){
+        for(int i = 0; i < this.count; i++){
+            if(empList[i] instanceof PieceWorkerEmployee){
+                ((PieceWorkerEmployee) empList[i]).displayInfo();
+            }
+        }
+    }
+
+    public void displayCE(){
+        for(int i = 0; i < this.count; i++){
+            if(empList[i] instanceof CommissionEmployee && !(empList[i] instanceof BasePlusCommissionEmployee)){
+                ((CommissionEmployee) empList[i]).displayInfo();
+            }
+        }
+    }
+
+    public void displayBPCE(){
+        for(int i = 0; i < this.count; i++){
+            if(empList[i] instanceof BasePlusCommissionEmployee){
+                ((BasePlusCommissionEmployee) empList[i]).displayInfo();
+            }
+        }
+    }
+
+    public boolean addEmployee(Employee e){
+        if(count >= max){
+            return false;
+        }
+        empList[count] = e;
+        this.count++;
+        return true;
+    }
+
+    public boolean deleteEmployee(int id){
+        for(int i = 0; i <= this.count; i++){
+            if(id == empList[i].getEmpId()){
+                for(int j = i; j <= this.count - 1; j++){
+                    empList[j] = empList[j+1];
+                }
+                empList[this.count - 1] = null;
+                --this.count;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public EmployeeRoster getEmployee(String name){
+        EmployeeRoster list = new EmployeeRoster(this.count);
+        for(int i = 0; i <= this.count; i++){
+            if(Objects.equals(name, empList[i].getEmpName().getFirstName())
+                    || Objects.equals(name, empList[i].getEmpName().getLastName())
+                    || Objects.equals(name, empList[i].getEmpName().getMiddleName()) ){
+                list.addEmployee(empList[i]);
+            }
+        }
+        return list;
+    }
+
+    // Update method for CommissionEmployee
+    public boolean updateEmployee(int id, Name newName, double totalSales) {
+        for (int i = 0; i < this.count; i++) {
+            if (id == empList[i].getEmpId() && empList[i] instanceof CommissionEmployee) {
+                CommissionEmployee commissionEmployee = (CommissionEmployee) empList[i];
+                commissionEmployee.setEmpName(newName);
+                commissionEmployee.setTotalSales(totalSales);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean updateEmployee(int id, Name newName, double rateOrSales, double additionalInfo) {
+        for (int i = 0; i < this.count; i++) {
+            if (id == empList[i].getEmpId()) {
+                empList[i].setEmpName(newName);
+
+                if (empList[i] instanceof HourlyEmployee) {
+                    HourlyEmployee hourlyEmployee = (HourlyEmployee) empList[i];
+                    hourlyEmployee.setRatePerHour((float)rateOrSales);
+                    hourlyEmployee.setTotalHoursWorked((float)additionalInfo);
+                    // Recalculate salary or perform other HourlyEmployee-specific operations
+                } else if (empList[i] instanceof BasePlusCommissionEmployee) {
+                    BasePlusCommissionEmployee basePlusEmployee = (BasePlusCommissionEmployee) empList[i];
+                    basePlusEmployee.setTotalSales(rateOrSales);
+                    basePlusEmployee.setBaseSalary(additionalInfo);
+                    // Recalculate salary or perform other BasePlusCommissionEmployee-specific operations
+                } else if (empList[i] instanceof PieceWorkerEmployee) {
+                    PieceWorkerEmployee pieceWorkerEmployee = (PieceWorkerEmployee) empList[i];
+                    pieceWorkerEmployee.setRatePerPiece((float)rateOrSales);
+                    pieceWorkerEmployee.setTotalPiecesFinished((int) additionalInfo);
+                    // Recalculate salary or perform other PieceWorkerEmployee-specific operations
+                }
+                return true;
+            }
+        }
+        return false;
     }
 }
--- EmployeeProject/src/employee/version4/Main.java
+++ EmployeeProject/src/employee/version4/Main.java
@@ -3,83 +3,101 @@ package employee.version4;
 public class Main {
     public static void main(String[] args) {
 
-//        //HOURLY EMPLOYEE
-//        HourlyEmployee hourlyE1 = new HourlyEmployee(1,new Name("Jericho", "Pasco", "Clam"), new Date(21,4,2020),
-//                new Date(31,12,2000), 60, 200);
-//        hourlyE1.displayInfo();
-//
-//        HourlyEmployee hourlyE2 = new HourlyEmployee(5, new Name("Nayeon", "Im", "Twice", "Senior"), new Date(24,6,2022),
-//                new Date(22,9,1995));
-//        hourlyE2.setTotalHoursWorked(70);
-//        hourlyE2.setRatePerHour(300);
-//        hourlyE2.displayInfo();
-//
-//        HourlyEmployee hourlyE3 = new HourlyEmployee();
-//        hourlyE3.setEmpId(6);
-//        hourlyE3.setEmpName(new Name("Ms", "Sana", "Minatozaki", "Twice", "Junior"));
-//        hourlyE3.setEmpDateHired(new Date(26,7,-2019));  //year is negative so set to default 2023
-//        hourlyE3.setEmpBirthDate(15,5,1997);
-//        hourlyE3.setTotalHoursWorked(45);
-//        hourlyE3.setRatePerHour(40);   //negative rate per hour so set to default 0
-//        hourlyE3.displayInfo();
-//
-//        //PIECE WORKER EMPLOYEE
-//        PieceWorkerEmployee pieceWorkerE1 = new PieceWorkerEmployee(2, new Name("John", "Pasco", "Clam"), new Date(22,3,2021),
-//                new Date(8,10,2002), 251, 60);
-//        pieceWorkerE1.displayInfo();
-//
-//        PieceWorkerEmployee pieceWorkerE2 = new PieceWorkerEmployee(7, new Name("Momo", "Hirai", "Twice", "Senior"), new Date(26,7,2023),
-//                new Date(9,11,1996));
-//        pieceWorkerE2.setRatePerPiece(50);
-//        pieceWorkerE2.setTotalPiecesFinished(312);
-//        pieceWorkerE2.displayInfo();
-//
-//        PieceWorkerEmployee pieceWorkerE3 = new PieceWorkerEmployee();
-//        pieceWorkerE3.setEmpId(8);
-//        pieceWorkerE3.setEmpName(new Name("Mr","Jeongyeon", "Yoo", "Twice", "II"));
-//        pieceWorkerE3.setEmpDateHired(new Date(20,10,-2015));  //year is negative so set to default 2023
-//        pieceWorkerE3.setEmpBirthDate(1,11,1996);
-//        pieceWorkerE3.setTotalPiecesFinished(105);
-//        pieceWorkerE3.setRatePerPiece(-60);             //negative rate per piece so set to default 0
-//        pieceWorkerE3.displayInfo();
-//
-//        //Commission Employee
-//        CommissionEmployee commissionE1 = new CommissionEmployee(3, new Name("Joe", "Pasco", "Clam"), new Date(19,5,2019),
-//                new Date(18,9,1996), 250000);
-//        commissionE1.displayInfo();
-//
-//        CommissionEmployee commissionE2 = new CommissionEmployee(9, new Name("Mina", "Myoui", "Twice", "III"), new Date(26,7,2023),
-//                new Date(24,3,1997));
-//        commissionE2.setTotalSales(90000);
-//        commissionE2.displayInfo();
-//
-//        CommissionEmployee commissionE3 = new CommissionEmployee();
-//        commissionE3.setEmpId(10);
-//        commissionE3.setEmpName(new Name("Dr", "Dahyun", "Kim", "Twice", "IV"));
-//        commissionE3.setEmpDateHired(20,20,-2015);          //month > 12 so set to default 1 and year < 0 set to default 2023
-//        commissionE3.setEmpBirthDate(new Date(28,5,1998));
-//        commissionE3.setTotalSales(900000);
-//        commissionE3.displayInfo();
-//
-//        //Base Plus Commission Employee
-//         BasePlusCommissionEmployee baseE1 = new BasePlusCommissionEmployee(4, new Name("Jihyo", "Park", "Twice"), new Date(18,8,2023),
-//                new Date(1,2,1997), 700000, 150000);
-//        baseE1.displayInfo();
-//
-//        BasePlusCommissionEmployee baseE2 = new BasePlusCommissionEmployee(11, new Name("Chaeyoung","Son", "Twice", "FOURTH"), new Date(20,10,2015),
-//                new Date(23,4,1999));
-//        baseE2.setTotalSales(80000);
-//        baseE2.setBaseSalary(200000);
-//        baseE2.displayInfo();
-//
-//        BasePlusCommissionEmployee baseE3 = new BasePlusCommissionEmployee();
-//        baseE3.setEmpId(12);
-//        baseE3.setEmpName(new Name("Lady","Tzuyu","Chou", "Twice", "FIFTH"));
-//        baseE3.setEmpDateHired(32,10,2015);         //day > 31 set to default 1
-//        baseE3.setEmpBirthDate(24,6,1999);
-//        baseE3.setTotalSales(800000);
-//        baseE3.setBaseSalary(-10000);                   //base salary < 0 set to default 0
-//        baseE3.displayInfo();
+        EmployeeRoster list = new EmployeeRoster(20);
 
+        //HOURLY EMPLOYEE
+        list.addEmployee(new HourlyEmployee(1,new Name("Jericho", "Pasco", "Clam"), new Date(21,4,2020),
+                new Date(31,12,2000), 60, 200));
+
+        list.addEmployee( new HourlyEmployee(5, new Name("Nayeon", "Im", "Twice", "Senior"), new Date(24,6,2022),
+                new Date(22,9,1995)));
+        ((HourlyEmployee)list.empList[list.getCount() - 1]).setTotalHoursWorked(70);
+        ((HourlyEmployee)list.empList[list.getCount() - 1]).setRatePerHour(300);
+
+        list.addEmployee(new HourlyEmployee());
+        ((HourlyEmployee)list.empList[list.getCount() - 1]).setEmpId(6);
+        ((HourlyEmployee)list.empList[list.getCount() - 1]).setEmpName(new Name("Ms", "Sana", "Minatozaki", "Twice", "Junior"));
+        ((HourlyEmployee)list.empList[list.getCount() - 1]).setEmpDateHired(new Date(26,7,-2019));  //year is negative so set to default 2023
+        ((HourlyEmployee)list.empList[list.getCount() - 1]).setEmpBirthDate(15,5,1997);
+        ((HourlyEmployee)list.empList[list.getCount() - 1]).setTotalHoursWorked(45);
+        ((HourlyEmployee)list.empList[list.getCount() - 1]).setRatePerHour(40);   //negative rate per hour so set to default 0
+
+        //PIECE WORKER EMPLOYEE
+        list.addEmployee( new PieceWorkerEmployee(2, new Name("John", "Pasco", "Clam"), new Date(22,3,2021),
+                new Date(8,10,2002), 251, 60));
+
+
+        list.addEmployee( new PieceWorkerEmployee(7, new Name("Momo", "Hirai", "Twice", "Senior"), new Date(26,7,2023),
+                new Date(9,11,1996)));
+        ((PieceWorkerEmployee)list.empList[list.getCount() - 1]).setRatePerPiece(50);
+        ((PieceWorkerEmployee)list.empList[list.getCount() - 1]).setTotalPiecesFinished(312);
+
+        list.addEmployee(new PieceWorkerEmployee());
+        ((PieceWorkerEmployee)list.empList[list.getCount() - 1]).setEmpId(8);
+        ((PieceWorkerEmployee)list.empList[list.getCount() - 1]).setEmpName(new Name("Mr","Jeongyeon", "Yoo", "Twice", "II"));
+        ((PieceWorkerEmployee)list.empList[list.getCount() - 1]).setEmpDateHired(new Date(20,10,-2015));  //year is negative so set to default 2023
+        ((PieceWorkerEmployee)list.empList[list.getCount() - 1]).setEmpBirthDate(1,11,1996);
+        ((PieceWorkerEmployee)list.empList[list.getCount() - 1]).setTotalPiecesFinished(105);
+        ((PieceWorkerEmployee)list.empList[list.getCount() - 1]).setRatePerPiece(-60);             //negative rate per piece so set to default 0
+
+        //Commission Employee
+        list.addEmployee(new CommissionEmployee(3, new Name("Joe", "Pasco", "Clam"), new Date(19,5,2019),
+                new Date(18,9,1996), 250000));
+
+       list.addEmployee(new CommissionEmployee(9, new Name("Mina", "Myoui", "Twice", "III"), new Date(26,7,2023),
+                new Date(24,3,1997)));
+        ((CommissionEmployee)list.empList[list.getCount() - 1]).setTotalSales(90000);
+
+       list.addEmployee(new CommissionEmployee());
+        ((CommissionEmployee)list.empList[list.getCount() - 1]).setEmpId(10);
+        ((CommissionEmployee)list.empList[list.getCount() - 1]).setEmpName(new Name("Dr", "Dahyun", "Kim", "Twice", "IV"));
+        ((CommissionEmployee)list.empList[list.getCount() - 1]).setEmpDateHired(20,20,-2015);          //month > 12 so set to default 1 and year < 0 set to default 2023
+        ((CommissionEmployee)list.empList[list.getCount() - 1]).setEmpBirthDate(new Date(28,5,1998));
+        ((CommissionEmployee)list.empList[list.getCount() - 1]).setTotalSales(900000);
+
+
+        //Base Plus Commission Employee
+         list.addEmployee(new BasePlusCommissionEmployee(4, new Name("Jihyo", "Park", "Twice"), new Date(18,8,2023),
+                new Date(1,2,1997), 700000, 150000));
+
+        list.addEmployee(new BasePlusCommissionEmployee(11, new Name("Chaeyoung","Son", "Twice", "FOURTH"), new Date(20,10,2015),
+                new Date(23,4,1999)));
+        ((BasePlusCommissionEmployee)list.empList[list.getCount() - 1]).setTotalSales(80000);
+        ((BasePlusCommissionEmployee)list.empList[list.getCount() - 1]).setBaseSalary(200000);
+
+        list.addEmployee(new BasePlusCommissionEmployee());
+        ((BasePlusCommissionEmployee)list.empList[list.getCount() - 1]).setEmpId(12);
+        ((BasePlusCommissionEmployee)list.empList[list.getCount() - 1]).setEmpName(new Name("Lady","Tzuyu","Chou", "Twice", "FIFTH"));
+        ((BasePlusCommissionEmployee)list.empList[list.getCount() - 1]).setEmpDateHired(32,10,2015);         //day > 31 set to default 1
+        ((BasePlusCommissionEmployee)list.empList[list.getCount() - 1]).setEmpBirthDate(24,6,1999);
+        ((BasePlusCommissionEmployee)list.empList[list.getCount() - 1]).setTotalSales(800000);
+        ((BasePlusCommissionEmployee)list.empList[list.getCount() - 1]).setBaseSalary(-10000);                   //base salary < 0 set to default 0
+
+        System.out.println("DISPLAY ALL EMPLOYEE");
+        list.displayAllEmployee();
+        System.out.println("Max number of employees = " + list.getMax()); //12
+        System.out.println("Current Count of total employees = " + list.getCount() + "\n"); //12
+
+        list.displayHE();
+        System.out.println("Count Hourly Employee = " + list.countHE() + "\n");
+        list.displayPWE();
+        System.out.println("Count Piece Worker Employee = " + list.countPWE() + "\n");
+        list.displayCE();
+        System.out.println("Count Commission Employee = " + list.countCE() + "\n");
+        list.displayBPCE();
+        System.out.println("Count Base Plus Commission Employee = " + list.countBPEC() + "\n");
+
+        list.deleteEmployee(3); //ID: - Joe C. Pasco delete
+        System.out.println("DISPLAY ALL EMPLOYEE AFTER DELETE Employee ID = 3");
+        list.displayAllEmployee();
+        System.out.println("Count of total employees after delete Employee = " + list.getCount() + "\n"); //11
+
+        list.updateEmployee(1, new Name("Jericho", "Clam", "Baldago"), 10, 20); // ID 1 - New middlename, lastname, rate per hour and total hours worked
+        list.updateEmployee(4, new Name("Jihyo", "Park", "Solo"), 800000, 250000); // ID 3 - New middle name, new totalsales, new base salaray
+        list.updateEmployee(7, new Name("Momo", "Hirai", "Misamo"), 100, 312); // ID 7 - New middle name and new rate per piece
+        list.updateEmployee(10, new Name("Dahyun", "Park", "Twice"), 500000); // ID 10 - New lastname and totalSales
+
+        System.out.println("DISPLAY ALL EMPLOYEE AFTER Update Employee IDs : 1, 4, 7, 10");
+        list.displayAllEmployee();
     }
 }
